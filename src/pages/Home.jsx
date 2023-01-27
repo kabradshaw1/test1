23cfd87449836f593cdfa6cad5523fe716f28355
kabradshaw1 <itllbok82@gmail.com>
2023-01-27 15:45:00 -0500

    I think this should be sending the json to the currect api for a post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,29 +7,36 @@ import Button from 'react-bootstrap/Button';
 const Home = () => {
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
 
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(userFormData)
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
     
-    // if (!emailInput || !passwordInput) {
-    //   return false;
-    // }
- 
-    // try {
-    //   await axios.post('api/user', )
-    // } catch(err) {
-    //   console.error(err)
-    // }
+    try {
+      await axios.post('api/user', userFormData)
+    } catch(err) {
+      console.error(err)
+    }
   };
 
   return (
     <Form className='bg-secondary' onSubmit={handleSubmit}>
-      <Form.Label><h3>User Form</h3></Form.Label>
+      <Form.Label><h3>Sign Up Form</h3></Form.Label>
       <Form.Group className='mb-3'>
         <Form.Label>Email address</Form.Label>
         <Form.Control name='email' value={userFormData.email} onChange={handleInputChange} type='text' placeholder='Enter email'/>

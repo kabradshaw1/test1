5130e3e00b97c6c313b05a85b564b8e0fb272e77
kabradshaw1 <itllbok82@gmail.com>
2023-01-28 16:39:09 -0500

    started login component

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert'
+import axios from 'axios';
+
+const Login = () => {
+
+  const handleSubmit = (event) => {
+
+  }
+
+  return (
+    <Form on submit={handleSubmit}>
+      
+    </Form>
+  )
+}
+
+export default Login;

7414b352db9937517c1240828dbfbe332d9bc5b0
kabradshaw1 <itllbok82@gmail.com>
2023-01-28 16:51:55 -0500

    added forgotten return to header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from './pages/Home'
 import Header from './components/Header'
 
+
 function App() {
 
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,26 +5,27 @@ import Row from 'react-bootstrap/Row';
 
 function Header(props) {
   const{ currentTab, handleTabChange } = props;
-
-  <Container fluid>
-    <Row className='align-items-center'>
-      <Col>Working App Name</Col>
-      <Col>
-        <Nav variant='pills'>
-          <Nav.Item>
-            <Nav.Link>
-              Option 1
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>
-              Option 2
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Col>
-    </Row>
-  </Container>
+  return(
+    <Container fluid>
+      <Row className='align-items-center'>
+        <Col>Working App Name</Col>
+        <Col>
+          <Nav variant='pills'>
+            <Nav.Item>
+              <Nav.Link>
+                Option 1
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                Option 2
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Header;

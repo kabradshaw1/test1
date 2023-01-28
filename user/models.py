507f27878cfcf46daf4247213a397136c1118dac
kabradshaw1 <itllbok82@gmail.com>
2023-01-28 16:48:30 -0500

    trying to add dev.to auth

--- user/models.py
+++ user/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class User(models.Model):
   email = models.CharField(max_length=15)
   password = models.CharField(max_length=15)
+  created = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return self.email
--- user/serializer.py
+++ user/serializer.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ('id', 'email', 'password')
+    fields = ('id', 'email', 'password', 'created')

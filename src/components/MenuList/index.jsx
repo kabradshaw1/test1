fb5d534f3e94ce1a5a2a5be7c68bd9b685df2f4a
kabradshaw1 <itllbok82@gmail.com>
2023-01-29 09:54:31 -0500

    menu api is working in browser

--- src/components/MenuList/index.jsx
+++ src/components/MenuList/index.jsx
@@ -13,7 +13,7 @@ export const MenuList = () => {
   }, [countRef]);
   const retrieveAllMenu = async () => {
     try{
-      let response = await axios.get('/menu/', {
+      let response = await axios.get('/api/menu/', {
         header: {
           headers
         }
@@ -23,4 +23,21 @@ export const MenuList = () => {
       console.error(err);
     }
   }
+  // This function and the one above are different methods of doing similar things.
+  const deleteMenu =(id) => {
+    axios
+      .delete(`/api/menu/${id}`, {
+        headers: {
+          headers
+        }
+      })
+      .then((response) =>{
+        setDeleted(true);
+        retrieveAllMenus();
+      })
+      .catch((e) => {
+        console.error(e);
+      })
+  }
+  
 }

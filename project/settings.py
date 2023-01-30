9d595e728df0e84a886eb3c8e31e88e2bb58f5a9
kabradshaw1 <itllbok82@gmail.com>
2023-01-30 16:35:38 -0500

    change settings

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['serene-tor-73175.herokuapp.com', '127.0.0.1:8000', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

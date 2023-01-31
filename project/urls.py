bcf3e3f7d544ce9c00242e0ea0e426d0674cb8aa
kabradshaw1 <itllbok82@gmail.com>
2023-01-31 07:51:27 -0500

    making route to render index.html from build

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from user.views import UserView
 from menu.views import MenuView
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'users', UserView, 'user')
@@ -11,5 +12,7 @@ router.register(r'menu', MenuView, basename='menu')
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/', include((router.urls, 'project'), namespace='project'))
+    path('api/', include((router.urls, 'project'), namespace='project')),
+    path('app/', include('user.urls'))
+
 ]
